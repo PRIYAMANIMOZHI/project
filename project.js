0f76ca9b979c07b95cdab60c8f5f1f67f0961918
Shanmugapriya Manimozhi <149061998+PRIYAMANIMOZHI@users.noreply.github.com>
2023-11-16 21:06:21 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,14 @@
+let search=document.querySelector('.searchbar');
+document.querySelector('#search').onclick=()=>
+{
+    search.classList.toggle('active');
+}
+
+
+let login=document.querySelector('.login');
+document.querySelector('#login').onclick=()=>
+{
+    login.classList.toggle('active');
+}
+
+
